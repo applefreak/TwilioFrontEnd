464990529c77b0982e516f6ae8cecb7e47562f1f
Po-Yu Chen <p820316@gmail.com>
2014-11-30 23:16:53 +0800

    Working on front page styling, ready to import js functions.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('index.html');
 });
 
-app.listen(3000);
+var server = app.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
